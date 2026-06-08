2162e29d71d7c99f8b40e2728cfdca9ec3e7e787
ToastPancake <soonhopark05@gmail.com>
2026-06-08 15:48:55 -0400

    chore: add docker deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@actual-app/api', 'better-sqlite3'],
+  output: 'standalone',
+  // Required since we are using 'better-sqlite3' and '@actual-app/api' which have native dependencies
+  serverExternalPackages: ['better-sqlite3', '@actual-app/api']
 };
 
 export default nextConfig;
